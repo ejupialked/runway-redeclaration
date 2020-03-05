096f4e254f58ffac87746c678e257ba940b0f9e6
bak1g18 <bak1g18@soton.ac.uk>
2020-03-05 20:21:18 +0000

    Added missing setName method

--- src/main/java/seg/team9/business/models/Obstacle.java
+++ src/main/java/seg/team9/business/models/Obstacle.java
@@ -27,6 +27,10 @@ public class Obstacle {
     public String getName() {
         return name;
     }
+    
+    public void setName(String name){
+        this.name = name;
+    }
 
     public void setHeight(Double height) {
         this.height = height;
