42132c2b79cced3574d5af5bbfbaf5c688611c2b
Alked <ae7g18@soton.ac.uk>
2020-05-13 12:00:30 +0200

    zero value message

--- src/main/java/seg/team9/exceptions/NegativeOrZeroNumberException.java
+++ src/main/java/seg/team9/exceptions/NegativeOrZeroNumberException.java
@@ -2,6 +2,6 @@ package seg.team9.exceptions;
 
 public class NegativeOrZeroNumberException extends Exception {
     public NegativeOrZeroNumberException(String name){
-        super("'" + name + "' has a negative value. Please insert a positive value.");
+        super("'" + name + "' has a negative or non-zero value. Please insert a positive value.");
     }
 }
